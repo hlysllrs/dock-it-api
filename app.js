9c42083df01b4d295b310d8d5c6bab17c22907e8
hlysllrs <hailey.sellars@gmail.com>
2023-07-01 23:44:48 -0400

    add routes to app.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const morgan = require('morgan')
-// INSERT ROUTES IMPORTS HERE
+
 const userRoutes = require('./routes/users')
 const teamRoutes = require('./routes/teams')
-// import projects routes
-// import tasks routes
+const projectRoutes = require('./routes/projects')
+const taskRoutes = require('./routes/tasks')
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('combined'))
-// INSERT MIDDLEWARE FOR ROUTES HERE
+
 app.use('/users', userRoutes)
 app.use('/teams', teamRoutes)
+app.use('/projects', projectRoutes)
+app.use('/tasks', taskRoutes)
 
 module.exports = app
